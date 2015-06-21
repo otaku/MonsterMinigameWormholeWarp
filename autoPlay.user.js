db814813c0e1a6d29d464f651429bee14dc6d89e
Alexander Do <alexanderdo@zormite.com>
2015-06-20 21:54:40 -0700

    Rewrite loader script

--- autoPlay.user.js
+++ autoPlay.user.js
@@ -2,67 +2,83 @@
 // @name Ye Olde Megajump (Auto-updating)
 // @namespace https://github.com/YeOldeWH/MonsterMinigameWormholeWarp
 // @description A script that runs the Steam Monster Minigame for you.  Now with megajump.  Brought to you by the Ye Olde Wormhole Schemers and DannyDaemonic
-// @version 6.0.2
+// @version 6.0.0
 // @match *://steamcommunity.com/minigame/towerattack*
 // @match *://steamcommunity.com//minigame/towerattack*
-// @grant       GM_xmlhttpRequest
+// @grant GM_xmlhttpRequest
 // @updateURL https://raw.githubusercontent.com/YeOldeWH/MonsterMinigameWormholeWarp/master/autoPlay.user.js
 // @downloadURL https://raw.githubusercontent.com/YeOldeWH/MonsterMinigameWormholeWarp/master/autoPlay.user.js
 // ==/UserScript==
 
-(function(x) {
+var loaderVersion = '6.0.0';
 
-// Options
-var old_version = false;
-var update_json_url = 'https://raw.githubusercontent.com/YeOldeWH/MonsterMinigameWormholeWarp/master/version.json';
-var script_url = 'https://raw.githubusercontent.com/YeOldeWH/MonsterMinigameWormholeWarp/master/autoplay.noUpdate.user.js';
-var loader_version = '5.0.2';
+function repoUser() {
+	var user = 'YeOldeWH';
 
-// Load the actual script
-GM_xmlhttpRequest ({
-    method: "GET",
-    url: script_url + "?" + new Date().getTime(),
-    onload: function(response) {
-        var scriptElement = document.createElement( "script" );
-        scriptElement.type = "text/javascript";
-        scriptElement.innerHTML = response.responseText;
-        document.body.appendChild (scriptElement);
-    }
-});        
+	try {
+		var repo = localStorage.getItem('steamdb-minigame-wormholers/githubRepo');
+		if (repo) {
+			user = repo;
+		}
+	} catch (e) {}
 
-function do_check () {
-    GM_xmlhttpRequest ({
-        method: "GET",
-        url: update_json_url + "?" + new Date().getTime(),
-        onload: function(response) {
-            var version_data = JSON.parse(response.responseText);
-            
-            if (version_data.Version != old_version) {
-                if (old_version === false) {
-                    // First time reading the JSON
-                    old_version = version_data.Version;
-
-                    // Display the version
-                    var versionDiv = document.createElement ("div");
-                    versionDiv.innerHTML = '<div style="' +
-                      'color: white; position: fixed; right: 1em; ' +
-                      'border: 1px solid white; padding: 2px; background: black;' + 
-                      'bottom: 1em; z-index: 9999;">Loader: ' + loader_version + ' Script: ' + old_version + '</div>';
-                    document.body.appendChild (versionDiv);
-
-                    // Check again in 15 minutes
-                    x.setTimeout (do_check, 15 * 60 * 1000);
-                } else {
-                    x.location.reload(true);
-                }
-            } else {
-                // Check again in 5 minutes
-                x.setTimeout (do_check, 5 * 60 * 1000);
-            }
-        }
-    });
-        
+	return user;
 }
 
-do_check ();
-}(window));
+function latestCommitSHA(callback) {
+	var user = repoUser();
+
+	GM_xmlhttpRequest({
+		method: 'GET',
+		url: 'https://api.github.com/repos/' + user + '/MonsterMinigameWormholeWarp/commits?v=' + new Date().getTime(),
+		onload: function (response) {
+			var commits = JSON.parse(response.responseText);
+
+			callback(commits && commits[0] && commits[0].sha ? commits[0].sha : null);
+		}
+	});
+}
+
+
+function checkForUpdates(currentVersion) {
+	function check() {
+		latestCommitSHA(function (sha) {
+			if (currentVersion !== sha) window.location.reload(true);
+		});
+
+		window.setTimeout(check, 5 * 60 * 1000);
+	}
+
+	// Display the version
+	var shortVersion = currentVersion.substr(0, 10);
+	var versionDiv = document.createElement('div');
+	versionDiv.innerHTML = '<div style="' +
+		'color: white; position: fixed; right: 1em; ' +
+		'border: 1px solid white; padding: 2px; background: black;' +
+		'bottom: 1em; z-index: 9999;">Loader: ' + loaderVersion + ' Script: ' + shortVersion + '</div>';
+	document.body.appendChild(versionDiv);
+
+	window.setTimeout(check, 15 * 60 * 1000);
+}
+
+
+function loadScript() {
+	var user = repoUser();
+
+	latestCommitSHA(function (sha) {
+		GM_xmlhttpRequest ({
+			method: 'GET',
+			url: 'https://raw.githubusercontent.com/' + user + '/MonsterMinigameWormholeWarp/' + sha + '/autoplay.noUpdate.user.js?v=' + new Date().getTime(),
+			onload: function (response) {
+				var scriptElement = document.createElement('script');
+				scriptElement.type = 'text/javascript';
+				scriptElement.innerHTML = response.responseText;
+				document.body.appendChild(scriptElement);
+			}
+		});
+
+		checkForUpdates(sha);
+	});
+}
+
+loadScript();
